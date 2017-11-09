56b819faf0390a77eb1e46f7f69fbc19833eb889
Mihyar Al Masalma <eng.mihyear@gmail.com>
2017-11-08 21:58:29 -0400

    Changing text to use dynamic strings

--- app/src/main/java/cs/dal/food4fit/LoginActivity.java
+++ app/src/main/java/cs/dal/food4fit/LoginActivity.java
@@ -1,12 +1,15 @@
 package cs.dal.food4fit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by Mihyar on 11/7/2017.
+ * Login activity
  */
 
 public class LoginActivity extends AppCompatActivity{
@@ -17,5 +20,10 @@ public class LoginActivity extends AppCompatActivity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goSignup(View view){
+        Intent signup_page = new Intent(this,LoginActivity.class);
+        startActivity(signup_page);
     }
 }
