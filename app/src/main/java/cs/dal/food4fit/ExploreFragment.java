938968daba903c236bd4daf99d8e0d064d985c98
Gongjia Liu <gn855506@dal.ca>
2017-11-19 23:02:49 -0400

    Replace ExploreFragment.java

--- app/src/main/java/cs/dal/food4fit/ExploreFragment.java
+++ app/src/main/java/cs/dal/food4fit/ExploreFragment.java
@@ -1,17 +1,30 @@
 package cs.dal.food4fit;
 
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ExploreFragment extends Fragment {
+
+
+    private GridView gridView;
+    private GridViewAdapter gridAdapter;
+    private Context context;
+
 
 
     public ExploreFragment() {
@@ -23,7 +36,29 @@ public class ExploreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false);
+
+
+        View view = inflater.inflate(R.layout.fragment_explore, container, false);
+
+        super.onCreate(savedInstanceState);
+
+        gridView = (GridView) view.findViewById(R.id.gridView);
+        gridAdapter = new GridViewAdapter(getActivity(), R.layout.grid_item_layout, getData());
+        gridView.setAdapter(gridAdapter);
+
+        return view;
+
+    }
+
+
+    private ArrayList<ImageItem> getData() {
+        final ArrayList<ImageItem> imageItems = new ArrayList<>();
+        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+        for (int i = 0; i < imgs.length(); i++) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
+            imageItems.add(new ImageItem(bitmap, "Image#" + i));
+        }
+        return imageItems;
     }
 
 }
