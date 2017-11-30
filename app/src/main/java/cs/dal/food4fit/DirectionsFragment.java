48b671f49d89ea2a85ebbc56c63a1ab3a7dfdb89
Gongjia Liu <gn855506@dal.ca>
2017-11-30 10:36:57 -0400

    Replace DirectionsFragment.java

--- app/src/main/java/cs/dal/food4fit/DirectionsFragment.java
+++ app/src/main/java/cs/dal/food4fit/DirectionsFragment.java
@@ -1,13 +1,21 @@
 package cs.dal.food4fit;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by graceliu on 2017-11-20.
@@ -16,20 +24,31 @@ import android.widget.Toast;
 public class DirectionsFragment extends Fragment {
     private static final String TAG = "DirectionsFragment";
 
-    private Button btnTEST2;
+    private String dir;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_directions,container,false);
-        btnTEST2 = (Button) view.findViewById(R.id.btnTEST2);
 
-        btnTEST2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 2",Toast.LENGTH_SHORT).show();
-            }
-        });
+        SharedPreferences sharedRecipe = getActivity().getSharedPreferences("Recipe", MODE_PRIVATE);
+        dir = sharedRecipe.getString("dir","default");
+
+//        TextView tvIngredients = (TextView)view.findViewById(R.id.directionsTab);
+//        tvIngredients.setText(dir);
+
+        String split[] = dir.split("(?<=[^ ]\\.) (?=[^a-z])");
+
+        ArrayList<String> directions = new ArrayList<String>();
+
+        for(int i=0;i<split.length;i++){
+            int t =i+1;
+            directions.add(t+". "+split[i]);
+        }
+
+        ListView listView2 = (ListView) view.findViewById(R.id.dirlist);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, directions);
+        listView2.setAdapter(adapter);
 
         return view;
     }
